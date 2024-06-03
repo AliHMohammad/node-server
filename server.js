1dd62e84da16d9c2652b3f1f8a8892f26788122c
AliHMohammad <alihmohammad@hotmail.com>
2024-06-03 23:02:49 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello eksamen! Let's have some fun with docker.");
+});
+
+app.listen(port, () => {
+    console.log("Listening on port " + port);
+});
---
+++ server/Dockerfile
@@ -0,0 +1,12 @@
+
+FROM node:18
+WORKDIR /demoapp
+
+COPY package.json ./
+COPY server.js ./
+
+RUN npm install
+
+CMD [ "node", "server.js" ]
+
+
