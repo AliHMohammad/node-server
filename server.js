ee32b77f3f9b045c6379c084eb62fbe8be7ef2db
Ali Mohammad <113783570+AliHMohammad@users.noreply.github.com>
2024-06-13 21:34:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello eksamen! Let's have some fun with docker.");
+    res.send("Hello eksamen! Let's have some fun with Terraform.");
 });
 
 app.listen(port, () => {
     console.log("Listening on port " + port);
-
-    
 });
